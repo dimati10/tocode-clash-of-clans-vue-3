28c7439671f595bf78f4d7f61ff3d60c08829b31
Dmitry <dmitrievd321@gmail.com>
2023-07-01 02:53:43 +0300

    initial commit

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  publicPath: '',
+  // publicPath: '/',
+  productionSourceMap: false,
+  assetsDir: 'assets'
+}
+
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
